39f084d4a978c6f47b146bfdb3be5ff08b10eca0
nicholassilvaa <vergueiroaroma@gmail.com>
2025-03-25 20:16:38 -0300

    created model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def price_show(self):
+        return "{:.2f}".format(self.price)
--- products/views.py
+++ products/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def home(request):
-    return HttpResponse('teste')
+    return render(request, 'home.html')
